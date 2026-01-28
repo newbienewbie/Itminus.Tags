7f601307b49f2effadf0cc1691136fe46a59e297
newbienewbie <itminus@hotmail.com>
2026-01-28 11:12:48 +0800

    增加通道加载器的实现

---
+++ src/Itminus.Tags/Projects/Channels/ChannelsLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itminus.Tags.Projects;
+
+internal class ChannelsLoader : IChannelsLoader
+{
+    private readonly IChannelFactory _channelFactory;
+
+    public ChannelsLoader(IChannelFactory channelFactory)
+    {
+        this._channelFactory = channelFactory;
+    }
+
+
+    /// <inheritdoc/>
+    public virtual IList<ITagChannel> LoadChannels(IList<ChannelDescriptor> descriptors)
+    {
+        var channels = descriptors
+            .Select(this._channelFactory.Create)
+            .ToList();
+        return channels;
+    }
+}
---
+++ src/Itminus.Tags/Projects/Channels/IChannelsLoader.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Itminus.Tags.Projects;
+
+
+/// <summary>
+/// 根据一系列通道描述符，加载 <see cref="ITagChannel"/>
+/// </summary>
+internal interface IChannelsLoader
+{
+    IList<ITagChannel> LoadChannels(IList<ChannelDescriptor> descriptors);
+}
