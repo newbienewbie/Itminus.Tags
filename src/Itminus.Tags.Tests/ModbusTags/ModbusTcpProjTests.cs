ca0cdc083e6c68c946f7fa35a2f7299c1b06b4b5
newbienewbie <itminus@hotmail.com>
2026-01-30 13:41:27 +0800

    增加Modbus项目加载测试

---
+++ src/Itminus.Tags.Tests/ModbusTags/ModbusTcpProjTests.cs
@@ -0,0 +1,111 @@
+﻿using Itminus.Tags.ModbusTcp;
+using Itminus.Tags.Projects;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Itminus.Tags.Tests.ModbusTags;
+
+public class ModbusTcpProjTests
+{
+    private readonly ServiceProvider _root;
+
+    public ModbusTcpProjTests()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddTagsProjectServices(b =>
+        {
+            b.AddModbusTcpSupport();
+        });
+
+        this._root = services.BuildServiceProvider();
+    }
+
+    [Fact]
+    public void TestLoad()
+    {
+        var scope = this._root.CreateScope();
+        var sp = scope.ServiceProvider;
+        var factory = sp.GetRequiredService<ITagsProjectFactory>();
+        var loc = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        var dir = System.IO.Path.GetDirectoryName(loc);
+        dir = Path.Combine(dir!, "ModbusTags");
+        var proj = factory.Create(dir!);
+
+        // Test Channels
+        Assert.Single(proj.Channels);
+        Assert.IsType<ModbusTcpChannel>(proj.Channels[0]);
+
+        // Test Tags
+        var g3 = proj.Tags.SelectGrp("g3");
+        Assert.NotNull(g3);
+
+
+        #region input group
+        // Verify Cache Size
+        var input = g3.SelectCbnt("输入");
+        Assert.Equal(2, input.CacheSize);
+        Assert.Equal(2, input.Cache.Length);
+
+        var btnLetGo = g3.SelectTag("输入/放行按钮闭合状态");
+        Assert.Equal("放行按钮闭合状态", btnLetGo.TagName());
+
+        var btnManual = g3.SelectTag("输入/手动");
+        Assert.Equal("手动", btnManual.TagName());
+
+        #endregion
+
+        #region output group
+        // Verify Cache Size
+        var output = g3.SelectCbnt("输出");
+        Assert.Equal(23, output.CacheSize);
+        Assert.Equal(23, output.Cache.Length);
+
+        var ledGreen = g3.SelectTag("输出/绿灯");
+        Assert.Equal("绿灯", ledGreen.TagName());
+
+        var ledRed = g3.SelectTag("输出/红灯");
+        Assert.Equal("红灯", ledRed.TagName());
+
+        var ledYellow = g3.SelectTag("输出/黄灯");
+        Assert.Equal("黄灯", ledYellow.TagName());
+
+        var ledLetGo = g3.SelectTag("输出/放行灯");
+        Assert.Equal("放行灯", ledLetGo.TagName());
+
+        #endregion
+
+        #region acquire group
+        // Verify Cache Size
+        var acq = g3.SelectCbnt("采集");
+        Assert.Equal(24 * 2 + 2, acq.CacheSize);
+        Assert.Equal(24 * 2 + 2, acq.Cache.Length);
+
+        var acq1 = acq.SelectTag("byte");
+        Assert.Equal("byte", acq1.TagName());
+        Assert.Equal("40020", acq1.TagDescriptor.Address);
+        Assert.Equal(TagKinds.BYTE, acq1.TagKind());
+
+        var acq2 = acq.SelectTag("int16");
+        Assert.Equal("int16", acq2.TagName());
+        Assert.Equal("40021", acq2.TagDescriptor.Address);
+        Assert.Equal(TagKinds.INT16, acq2.TagKind());
+
+        var acq3 = acq.SelectTag("int32");
+        Assert.Equal("int32", acq3.TagName());
+        Assert.Equal("40022", acq3.TagDescriptor.Address);
+        Assert.Equal(TagKinds.INT32, acq3.TagKind());
+
+        var acq4 = acq.SelectTag("float");
+        Assert.Equal("float", acq4.TagName());
+        Assert.Equal("40024", acq4.TagDescriptor.Address);
+        Assert.Equal(TagKinds.FLOAT, acq4.TagKind());
+        #endregion
+    }
+}
