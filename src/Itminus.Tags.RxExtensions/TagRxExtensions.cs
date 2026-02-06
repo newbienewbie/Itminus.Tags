00cce82c1aba7764f82fae90eba502f7fdc9858c
newbienewbie <itminus@hotmail.com>
2026-02-06 14:33:37 +0800

    完善注释

--- src/Itminus.Tags.RxExtensions/TagRxExtensions.cs
+++ src/Itminus.Tags.RxExtensions/TagRxExtensions.cs
@@ -5,6 +5,11 @@ namespace Itminus.Tags;
 
 public static class TagRxExtensions
 {
+    /// <summary>
+    /// 观测从底层读取的事件
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
     public static IObservable<EventPattern<ITag, TagSyncEventArgs>> ObserveOnRead(this ITag tag)
     {
         var read = Observable.FromEventPattern<TagSyncEventHandler, ITag, TagSyncEventArgs>(
@@ -14,6 +19,11 @@ public static class TagRxExtensions
         return read;
     }
 
+    /// <summary>
+    /// 观测向底层写入的事件
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
     public static IObservable<EventPattern<ITag, TagSyncEventArgs>> ObserveOnWritten(this ITag tag)
     {
         var written = Observable.FromEventPattern<TagSyncEventHandler, ITag, TagSyncEventArgs>(
@@ -23,16 +33,27 @@ public static class TagRxExtensions
         return written;
     }
 
-
-    public static IObservable<EventPattern<ITag, TagSyncEventArgs>> Watch(this ITag tag)
+    /// <summary>
+    /// 观测从底层读取或者向底层写入事件
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static IObservable<EventPattern<ITag, TagSyncEventArgs>> Watch(this ITag tag, bool startWithCurrent=true)
     {
         var read = tag.ObserveOnRead();
         var written = tag.ObserveOnWritten();
         var obs = read.Merge(written);
-        var ev = new EventPattern<ITag,TagSyncEventArgs>(
-            tag, 
-            new TagSyncEventArgs(tag.Value, tag.Timestamp, TagSyncEventArgs.Kinds.None)
-            );
-        return obs.StartWith(ev);
+        if(startWithCurrent)
+        {
+            var ev = new EventPattern<ITag, TagSyncEventArgs>(
+                tag,
+                new TagSyncEventArgs(tag.Value, tag.Timestamp, TagSyncEventArgs.Kinds.None)
+                );
+            return obs.StartWith(ev);
+        }
+        else
+        {
+            return obs;
+        }
     }
 }
