59ff61d195a728facfb71e66f57512b989d8a964
newbienewbie <itminus@hotmail.com>
2026-02-07 13:40:27 +0800

    TagGrpRunner: 规范轮询崩溃机制
    这个运行期运行机制类似于一个PLC。它遵循:  [读取输入]->[处理]、[输出]。
    
    一旦成功启动循环，除非主动取消或者被异常处理的二次异常打断，否则不会停止。

--- src/Itminus.Tags/TagGrpRunner/ITagGrpRunner.cs
+++ src/Itminus.Tags/TagGrpRunner/ITagGrpRunner.cs
@@ -28,7 +28,11 @@ public delegate Task TurnCrashed(ITagGrp grp, ITagChannel channel, Exception ex)
 
 
 /// <summary>
-/// 测点群组监控
+/// 测点群组运行器。<br/>
+/// 这个运行期运行机制类似于一个PLC。它遵循:<br/>
+///     <b>读取输入</b>-&gt;<b>处理</b>-&gt;<b>输出</b> 
+/// 的循环。<br/>
+/// 一旦成功启动循环，除非主动取消或者被异常处理的二次异常打断，否则不会停止。
 /// </summary>
 public interface ITagGrpRunner
 {
@@ -44,12 +48,14 @@ public interface ITagGrpRunner
     event TurnProcess? TurnProcess;
 
     /// <summary>
-    /// 崩溃
+    /// 崩溃处理。<br/>
+    /// 注意崩溃处理中不要再有异常发生，否则会打断轮询
     /// </summary>
     event TurnCrashed? TurnCrashed;
 
     /// <summary>
     /// 启动对群组的监控: loop(输入 -> 处理 ->输出)<br/>
+    /// 一旦成功启动轮询，除非主动取消，否则不会抛出异常。
     /// </summary>
     /// <param name="entry"></param>
     /// <param name="ct"></param>
---
+++ src/Itminus.Tags/TagGrpRunner/ITagGrpRunnerFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Itminus.Tags;
+
+
+/// <summary>
+/// <see cref="ITagGrpRunner"/> 工厂
+/// </summary>
+public interface ITagGrpRunnerFactory
+{
+    /// <summary>
+    /// 创建一个<see cref="ITagGrpRunner"/>实例
+    /// </summary>
+    /// <returns></returns>
+    ITagGrpRunner Create();
+}
