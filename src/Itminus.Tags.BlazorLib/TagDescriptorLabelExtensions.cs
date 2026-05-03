2d28bdc2e401d3f35de7b58ab6b0cc0dcf3c9932
newbienewbie <itminus@hotmail.com>
2026-05-03 19:53:10 +0800

    MVP

---
+++ src/Itminus.Tags.BlazorLib/TagDescriptorLabelExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Itminus.Tags.BlazorLib;
+
+internal static class TagDescriptorLabelExtensions
+{
+    public static string? GetLabel(this ITagsDescriptor? d)
+    {
+        return d is null ? null :
+             d is TagGrpDescriptor grpDescriptor ? grpDescriptor.Name :
+             d is TagCbntDescriptor cbntDescriptor ? cbntDescriptor.Name :
+             d is TagDescriptor descriptor ? descriptor.TagName :
+             d.ToString();
+    }
+}
