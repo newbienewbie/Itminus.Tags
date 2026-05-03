218eae3c53b52743b7b02173cdaa0db20a94dc52
newbienewbie <itminus@hotmail.com>
2026-05-03 10:02:21 +0800

    重构： 剥离各组件实现的注册

--- src/Itminus.Tags.BlazorLib/ServiceCollectionExtensions.cs
+++ src/Itminus.Tags.BlazorLib/ServiceCollectionExtensions.cs
@@ -1,12 +1,9 @@
 ﻿using Itminus.Tags.BlazorLib.Components.Channels.Impl.OpcUa;
 using Itminus.Tags.BlazorLib.Components.Channels.Impl.ZLan;
-using Itminus.Tags.BlazorLib.Components.Channels.Editing;
 using Itminus.Tags.BlazorLib.Components.Channels.Impl.ComScanner;
 using Itminus.Tags.BlazorLib.Components.Channels.Impl.Hjzk;
 using Itminus.Tags.BlazorLib.Components.Channels.Impl.ModbusTcp;
 using Itminus.Tags.BlazorLib.Components.Channels.Impl.S7;
-
-using Itminus.Tags.BlazorLib.Components.Tags.Editing;
 using Itminus.Tags.BlazorLib.Components.Tags.Editors;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,53 +11,28 @@ namespace Itminus.Tags.BlazorLib;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddTagsBlazorLib(this IServiceCollection services)
+    public static IServiceCollection AddTagsBlazorLib(this IServiceCollection services, Action<TagsBlazorBuilder>? config = null)
     {
-        services.AddScoped<TagsBlazorLibJsInterop>();
-        // Registry
-        services.AddScoped<TagValueEditorRegistry>();
-        services.AddScoped<TagChannelDescriptorRendererRegistry>();
-
-        services.AddTagValueEditor<BoolTagValueEditor>()
-                .AddTagValueEditor<NumericTagValueEditor>()
-                .AddTagValueEditor<TextTagValueEditor>();
+        var builder = new TagsBlazorBuilder(services);
+        // default tag value editors
+        builder.AddTagValueEditor<BoolTagValueEditor>()
+               .AddTagValueEditor<NumericTagValueEditor>()
+               .AddTagValueEditor<TextTagValueEditor>();
 
         // Default channel descriptor viewers/editors
-        services.AddTagChannelDescriptorViewer<S7TagChannelDescriptorViewer>()
-                .AddTagChannelDescriptorEditor<S7TagChannelDescriptorEditor>()
-                .AddTagChannelDescriptorViewer<ModbusTcpTagChannelDescriptorViewer>()
-                .AddTagChannelDescriptorEditor<ModbusTcpTagChannelDescriptorEditor>()
-                .AddTagChannelDescriptorViewer<HjzkTagChannelDescriptorViewer>()
-                .AddTagChannelDescriptorEditor<HjzkTagChannelDescriptorEditor>()
-                .AddTagChannelDescriptorViewer<ComScannerTagChannelDescriptorViewer>()
-                .AddTagChannelDescriptorEditor<ComScannerTagChannelDescriptorEditor>()
-                .AddTagChannelDescriptorViewer<ZLanTcpTagChannelDescriptorViewer>()
-                .AddTagChannelDescriptorEditor<ZLanTcpTagChannelDescriptorEditor>()
-                .AddTagChannelDescriptorViewer<OpcUaClientTagChannelDescriptorViewer>()
-                .AddTagChannelDescriptorEditor<OpcUaClientTagChannelDescriptorEditor>();
+        builder.AddS7ChannelDescriptorViewerAndEditor()
+               .AddModbusTcpChannelDescriptorViewerAndEditor()
+               .AddHjzkChannelDescriptorViewerAndEditor()
+               .AddComScannerChannelDescriptorViewerAndEditor()
+               .AddZLanChannelDescriptorViewerAndEditor()
+               .AddOpcUaChannelDescriptorViewerAndEditor();
 
-        return services;
+        // user configuration
+        config?.Invoke(builder);
+        builder.Build();
+        return builder.Services;
     }
 
 
-    public static IServiceCollection AddTagValueEditor<TTagValueEditor>(this IServiceCollection services)
-        where TTagValueEditor:class, ITagValueEditor
-    {
-        services.AddScoped<ITagValueEditor, TTagValueEditor>();
-        return services;
-    }
 
-    public static IServiceCollection AddTagChannelDescriptorViewer<TViewer>(this IServiceCollection services)
-        where TViewer : class, ITagChannelDescriptorViewer
-    {
-        services.AddScoped<ITagChannelDescriptorViewer, TViewer>();
-        return services;
-    }
-
-    public static IServiceCollection AddTagChannelDescriptorEditor<TEditor>(this IServiceCollection services)
-        where TEditor : class, ITagChannelDescriptorEditor
-    {
-        services.AddScoped<ITagChannelDescriptorEditor, TEditor>();
-        return services;
-    }
 }
---
+++ src/Itminus.Tags.BlazorLib/TagsBlazorBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Itminus.Tags.BlazorLib.Components.Channels.Editing;
+
+using Itminus.Tags.BlazorLib.Components.Tags.Editing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Itminus.Tags.BlazorLib;
+
+public class TagsBlazorBuilder
+{
+    public IServiceCollection Services { get; }
+    public TagsBlazorBuilder(IServiceCollection services)
+    {
+        services.AddScoped<TagsBlazorLibJsInterop>();
+        // Registry
+        services.AddScoped<TagValueEditorRegistry>();
+        services.AddScoped<TagChannelDescriptorRendererRegistry>();
+
+        Services = services;
+    }
+
+    public TagsBlazorBuilder AddTagValueEditor<TEditor>()
+        where TEditor : class, ITagValueEditor
+    {
+        this.Services.AddScoped<ITagValueEditor, TEditor>();
+        return this;
+    }
+
+    public TagsBlazorBuilder AddTagChannelDescriptorViewer<TViewer>()
+        where TViewer : class, ITagChannelDescriptorViewer
+    {
+        this.Services.AddScoped<ITagChannelDescriptorViewer, TViewer>();
+        return this;
+    }
+
+    public TagsBlazorBuilder AddTagChannelDescriptorEditor<TEditor>()
+        where TEditor : class, ITagChannelDescriptorEditor
+    {
+        this.Services.AddScoped<ITagChannelDescriptorEditor, TEditor>();
+        return this;
+    }
+
+
+    public void Build()
+    {
+        return;
+    }
+}
