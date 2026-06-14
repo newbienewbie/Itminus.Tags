eb3aa801b697238fb1062f5847a2658ea6dbcc7f
newbienewbie <itminus@hotmail.com>
2026-06-14 19:03:37 +0800

    cleanup

--- src/Itminus.Tags.S7/DirectTags/DirectTagBuilder.cs
+++ src/Itminus.Tags.S7/DirectTags/DirectTagBuilder.cs
@@ -16,13 +16,6 @@ internal partial class S7DirectTagBuilder : TagBuilderBase
         }
 
         var factory = new S7DirectTagFactory(this.Parent.IntoTagContainer());
-       
-        // 这个时候 tag 尚未构造完成，尚未关联到 Parent，不可以使用 tag.GetRequiredChannel()，必须依赖外部传入的 channel 参数
-        var s7Channel = channel as S7TagChannel;
-        if(s7Channel is null)
-        {
-            throw new Exception($"测点({this.Name})配置的通道不是{nameof(S7TagChannel)}");
-        }
         var tag = factory.Create(this.TagDescriptor, this.Channel);
 
         return tag;
