7362f33cb0790be94c192c988301f85011503823
newbienewbie <itminus@hotmail.com>
2026-06-15 13:52:47 +0800

    cleanup

--- src/Itminus.Tags.Core/Tags/Tag.cs
+++ src/Itminus.Tags.Core/Tags/Tag.cs
@@ -8,7 +8,6 @@ namespace Itminus.Tags;
 /// </summary>
 /// <typeparam name="TValue"></typeparam>
 public abstract class Tag<TValue,TChannel> : ITag
-    //where TValue : IEquatable<TValue>
     where TChannel: class, ITagChannel
 {
 
