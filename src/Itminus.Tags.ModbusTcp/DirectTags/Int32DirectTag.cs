a77e2311b38d16701dd1fa57865e195af8df7bbe
newbienewbie <itminus@hotmail.com>
2026-06-15 09:58:18 +0800

    完善Modbus测试

--- src/Itminus.Tags.ModbusTcp/DirectTags/Int32DirectTag.cs
+++ src/Itminus.Tags.ModbusTcp/DirectTags/Int32DirectTag.cs
@@ -3,7 +3,7 @@ using System.Buffers.Binary;
 
 namespace Itminus.Tags.ModbusTcp;
 
-public class UInt32DirectTag : MutileBytesDirectTag<Int32>
+public class UInt32DirectTag : MutileBytesDirectTag<uint>
 {
     public UInt32DirectTag(TagDescriptor descriptor, ModbusTcpChannel? thisChannel, TagContainer container)
         : base(descriptor, thisChannel, container)
@@ -12,27 +12,27 @@ public class UInt32DirectTag : MutileBytesDirectTag<Int32>
 
     protected override int BufferSize => 4;
 
-    protected override void FillBytes(Int32 value, in Span<byte> buffer)
+    protected override void FillBytes(uint value, in Span<byte> buffer)
     {
         switch (this.TagDescriptor.EndianKind)
         {
             case EndianKinds.BigEndian:
-                BinaryPrimitives.WriteInt32BigEndian(buffer, value);
+                BinaryPrimitives.WriteUInt32BigEndian(buffer, value);
                 break;
             case EndianKinds.LittleEndian:
-                BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+                BinaryPrimitives.WriteUInt32LittleEndian(buffer, value);
                 break;
             default:
                 throw new InvalidOperationException($"不支持的字节序类型: {this.TagDescriptor.EndianKind}");
         }
     }
 
-    protected override int GetValueFromBytes(byte[] bytes)
+    protected override uint GetValueFromBytes(byte[] bytes)
     {
         return this.TagDescriptor.EndianKind switch
         {
-            EndianKinds.BigEndian => BinaryPrimitives.ReadInt32BigEndian(bytes),
-            EndianKinds.LittleEndian => BinaryPrimitives.ReadInt32LittleEndian(bytes),
+            EndianKinds.BigEndian => BinaryPrimitives.ReadUInt32BigEndian(bytes),
+            EndianKinds.LittleEndian => BinaryPrimitives.ReadUInt32LittleEndian(bytes),
             _ => throw new InvalidOperationException($"不支持的字节序类型: {this.TagDescriptor.EndianKind}")
         }; 
     }
