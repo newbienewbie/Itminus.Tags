9b353a2659322fc864ae2c1bc28d32afac901654
newbienewbie <itminus@hotmail.com>
2026-07-01 11:13:25 +0800

    fix: OpcUA 组合子赋值时应使用value而不是旧的DataValue

--- src/Itminus.Tags.Web/Logicets/HandleSnap11.cs
+++ src/Itminus.Tags.Web/Logicets/HandleSnap11.cs
@@ -13,18 +13,15 @@ internal class HandleSnap11 : LogicetBase
     private ITagCbnt _cbnt2;
     private ITagCbntor _reqTag;
     private ITagCbntor _ackTag;
-    private ITagCbntor _mat;
-    private ITagCbntor _prog;
+
 
     public HandleSnap11(IReadOnlyList<ITagChannel> channels, ITagGrp tags) : base(channels, tags)
     {
-        this._cbnt1 = this.Tags.SelectCbnt("g1/拍照请求");
-        this._reqTag = _cbnt1.SelectTag("拍照-请求-标志");
-        this._mat = _cbnt1["拍照-请求-料号"];
-        this._prog = _cbnt1["拍照-请求-程序号"];
+        this._cbnt1 = this.Tags.SelectCbnt("g4/输入");
+        this._reqTag = _cbnt1.SelectTag("心跳请求");
 
-        this._cbnt2 = this.Tags.SelectCbnt("g1/拍照响应");
-        this._ackTag = _cbnt2.SelectTag("拍照-响应-标志");
+        this._cbnt2 = this.Tags.SelectCbnt("g4/输出");
+        this._ackTag = _cbnt2.SelectTag("心跳响应");
     }
 
     public override int Order => 1;
@@ -32,19 +29,16 @@ internal class HandleSnap11 : LogicetBase
 
     public override bool MatchEntry(ITagGrp entry)
     {
-        return entry.Name == "g1";
+        return entry.Name == "g4";
     }
 
     public override Task ProcessAsync(ITagGrp entry, ITagChannel? thisChannel)
     {
         var hasReq = this._reqTag.GetTagValue<bool>();
-        var hasAck = _ackTag.GetTagValue<bool>();
+        var hasAck = _ackTag.GetTagValue<bool?>() == true;
 
         if (hasReq && !hasAck)
         {
-            var matcode = this._mat.GetTagValue<byte>();
-            var progNo = this._prog.GetTagValue<short>();
-            Console.WriteLine($"拍照响应：料号={matcode}，程序号={progNo}");
             _ackTag.Value = true;
         }
 
