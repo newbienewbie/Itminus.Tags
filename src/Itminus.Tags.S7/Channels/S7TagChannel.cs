4c81b00060cf38218de0987ef438ba2ba26bb0bd
newbienewbie <itminus@hotmail.com>
2026-07-16 18:05:16 +0800

    S7增加连接、断开连接、读、写的并发控制

--- src/Itminus.Tags.S7/Channels/S7TagChannel.cs
+++ src/Itminus.Tags.S7/Channels/S7TagChannel.cs
@@ -3,6 +3,7 @@ using StdUnit.Sharp7.Options;
 using StdUnit.Sharp7;
 using Microsoft.FSharp.Core;
 
+
 namespace Itminus.Tags.S7;
 
 
@@ -22,6 +23,9 @@ public class S7TagChannel : IContinousBytesBasedTagChannel
     }
 
     private readonly ILogger<S7TagChannel> _logger;
+    private readonly SemaphoreSlim _rw = new SemaphoreSlim(1, 1);
+
+
 
     internal S7Client? Client { get; set; }
     public string ChannelName { get; set; } = DRIVER;
@@ -31,45 +35,77 @@ public class S7TagChannel : IContinousBytesBasedTagChannel
 
     private static readonly string DRIVER = S7Names.DriverName;
 
-    public virtual Task DisconnectAsync(CancellationToken ct)
+    public virtual async Task DisconnectAsync(CancellationToken ct)
     {
-        if (this.Client == null)
-            return Task.CompletedTask;
+        await this.ExecuteOneByOneAsync(
+            async ct =>
+            {
+                if (this.Client == null)
+                    return;
 
-        var tcs = new TaskCompletionSource<Object?>();
-        var th = new Thread(() => {
-            try
-            {
-                this.Client?.Disconnect();
-                this.Client = null;
-                tcs.SetResult(null);
-            }
-            catch (Exception ex)
-            {
-                this._logger.LogWarning("通道{ChannelName}断开连接失败：{message}\r\n{stackTrace}", ChannelName, ex.Message, ex.StackTrace);
-                this.Client = null;
-                tcs.SetException(ex);
-            }
-        });
-        th.Start();
-        return tcs.Task;
+                var tcs = new TaskCompletionSource<Object?>();
+                var th = new Thread(() => {
+                    try
+                    {
+                        this.Client?.Disconnect();
+                        this.Client = null;
+                        tcs.SetResult(null);
+                    }
+                    catch (Exception ex)
+                    {
+                        this._logger.LogWarning("通道{ChannelName}断开连接失败：{message}\r\n{stackTrace}", ChannelName, ex.Message, ex.StackTrace);
+                        this.Client = null;
+                        tcs.SetException(ex);
+                    }
+                });
+                th.Start();
+                await tcs.Task;
+            },
+            ct
+        );
     }
+
 
     public virtual async Task EnsureConnectedAsync(bool force, CancellationToken ct)
     {
-        //当前client存在并且连接有效
-        if (!force && Client != null && Client.Connected)
-        {
-            return;
-        }
+        await this.ExecuteOneByOneAsync(
+            async ct => {
+                //当前client存在并且连接有效
+                if (!force && Client != null && Client.Connected)
+                {
+                    return;
+                }
 
-        var result = await this.CreateClientAndConnectAsync(ct);
-        if (result.IsError)
-        {
-            throw new Exception(result.ErrorValue.ToString());
-        }
-        this.Client = result.ResultValue;
+                // 如果要强制连接，先清理当前的连接
+                if (force && Client != null)
+                {
+                    try
+                    {
+                        this._logger.LogInformation("通道={ChannelName} 强制断开连接中...", ChannelName);
+                        this.Client.Disconnect();
+                    }
+                    catch(Exception ex)
+                    {
+                        this._logger.LogError(ex, "通道={ChannelName} 强制断开异常", ChannelName);
+                    }
+                    finally
+                    {
+                        this.Client = null;
+                    }
+                }
+
+                var result = await this.CreateClientAndConnectAsync(ct);
+                if (result.IsError)
+                {
+                    throw new Exception(result.ErrorValue.ToString());
+                }
+                this.Client = result.ResultValue;
+            },
+            ct
+        );
     }
+
+
 
     protected virtual Task<FSharpResult<S7Client, ApiError>> CreateClientAndConnectAsync(CancellationToken ct)
     {
@@ -105,34 +141,47 @@ public class S7TagChannel : IContinousBytesBasedTagChannel
     /// <param name="length">要读取的字节数量</param>
     /// <returns></returns>
     /// <exception cref="Exception"></exception>
-    public virtual Task<byte[]> ReadAsync(string address, int length, CancellationToken ct)
+    public virtual async Task<byte[]> ReadAsync(string address, int length, CancellationToken ct)
     {
         var addr = S7AddressParser.Parse(address);
         var buffer = new byte[length];
         if (addr.Area == AreaKinds.DB)
         {
-            var code = this.Client!.DBRead(addr.BlockNumber, addr.StartAddress, length, buffer);
-            if (code != 0)
-            {
-                var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
-                throw new Exception(err.Text);
-            }
+            await this.ExecuteOneByOneAsync(
+                ct => {
+                    var client = this.Client ?? throw new InvalidOperationException("S7通道客户端为null");
+                    var code = client.DBRead(addr.BlockNumber, addr.StartAddress, length, buffer);
+                    if (code != 0)
+                    {
+                        var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
+                        throw new Exception(err.Text);
+                    }
+                    return Task.CompletedTask;
+                },
+                ct
+            );
         }
         else if (addr.Area == AreaKinds.MB) 
-        { 
-            var code = this.Client!.MBRead(addr.StartAddress, length, buffer);
-            if(code != 0)
-            {
-                var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
-                throw new Exception(err.Text);
-            }
+        {
+            await this.ExecuteOneByOneAsync(
+                ct => {
+                    var client = this.Client ?? throw new InvalidOperationException("S7通道客户端为null");
+                    var code = client.MBRead(addr.StartAddress, length, buffer);
+                    if (code != 0)
+                    {
+                        var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
+                        throw new Exception(err.Text);
+                    }
+                    return Task.CompletedTask;
+                },
+                ct
+            );
         }
         else
         {
             throw new NotImplementedException($"不支持的地址区域类型={addr.Area}");
         }
-
-        return Task.FromResult(buffer);
+        return buffer;
     }
 
     /// <summary>
@@ -142,53 +191,96 @@ public class S7TagChannel : IContinousBytesBasedTagChannel
     /// <param name="buffer"></param>
     /// <returns></returns>
     /// <exception cref="Exception"></exception>
-    public virtual Task WriteAsync(string address, byte[] buffer, CancellationToken ct)
+    public virtual async Task WriteAsync(string address, byte[] buffer, CancellationToken ct)
     {
         var addr = S7AddressParser.Parse(address);
         if(addr.Area == AreaKinds.DB)
         {
-            var code = this.Client!.DBWrite(addr.BlockNumber, addr.StartAddress, buffer.Length, buffer);
-            if (code != 0)
-            {
-                var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
-                throw new Exception(err.Text);
-            }
+            await this.ExecuteOneByOneAsync(
+                ct => {
+                    var client = this.Client ?? throw new InvalidOperationException("S7通道客户端为null");
+                    var code = client.DBWrite(addr.BlockNumber, addr.StartAddress, buffer.Length, buffer);
+                    if (code != 0)
+                    {
+                        var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
+                        throw new Exception(err.Text);
+                    }
+                    return Task.CompletedTask;
+                },
+                ct
+            );
         }
         else if(addr.Area == AreaKinds.MB)
         {
-            var code = this.Client!.MBWrite(addr.StartAddress, buffer.Length, buffer);
-            if (code != 0)
-            {
-                var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
-                throw new Exception(err.Text);
-            }
+            await this.ExecuteOneByOneAsync(
+                ct => {
+                    var client = this.Client ?? throw new InvalidOperationException("S7通道客户端为null");
+                    var code = client.MBWrite(addr.StartAddress, buffer.Length, buffer);
+                    if (code != 0)
+                    {
+                        var err = S7ErrorCodeHelper.GenerateApiError(this.ChannelName, code);
+                        throw new Exception(err.Text);
+                    }
+                    return Task.CompletedTask;
+                },
+                ct
+            );
         }
         else
         {
             throw new NotImplementedException($"不支持的地址区域类型={addr.Area}");
         }
-        return Task.CompletedTask;
     }
 
 
     public virtual void Dispose()
     {
-        if (Client != null && Client.Connected)
+        var client = this.Client;
+        if (client != null && client.Connected)
         {
             try
             {
-                this._logger.LogInformation("通道={ChannelName} 正在断开连接...", ChannelName);
-                Client.Disconnect();
-                this._logger.LogInformation("通道={ChannelName}  断开连接完成!", ChannelName);
+                this._logger.LogInformation("通道={ChannelName} 正在释放: 断开连接中...", ChannelName);
+
+                this.ExecuteOneByOne(() =>
+                {
+                    client.Disconnect();
+                    this._logger.LogInformation("通道={ChannelName} 正在释放: 断开连接完成!", ChannelName);
+                });
             }
             catch (Exception e)
             {
                 this._logger.LogWarning("通道={ChannelName} 释放异常:{exception}", ChannelName, e.Message);
             }
-            finally
-            {
+        }
+        this.Client = null;
+        this._rw.Dispose();
+    }
 
-            }
+
+    private async Task ExecuteOneByOneAsync(Func<CancellationToken, Task> action, CancellationToken ct)
+    {
+        await this._rw.WaitAsync(ct);
+        try
+        {
+            await action(ct);
+        }
+        finally
+        {
+            this._rw.Release();
+        }
+    }
+
+    private void ExecuteOneByOne(Action action)
+    {
+        this._rw.Wait();
+        try
+        {
+            action();
+        }
+        finally
+        {
+            this._rw.Release();
         }
     }
 }
