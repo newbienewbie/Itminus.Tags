e8bddcc18a0ddcb096508087f30bffa4df71366f
newbienewbie <itminus@hotmail.com>
2026-07-16 21:14:54 +0800

    增加MakeProject扩展方法

--- src/Itminus.Tags/ServiceExtensions/TagsProjectServiceCollection.cs
+++ src/Itminus.Tags/ServiceExtensions/TagsProjectServiceCollection.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace Itminus.Tags;
 
@@ -18,4 +20,30 @@ public static class TagsProjectServiceCollection
         tpsb.Build();
         return services;
     }
+
+
+    /// <summary>
+    /// 构建测点项目实例
+    /// </summary>
+    /// <param name="sp"></param>
+    /// <param name="dir">如果为空，则使用当前程序集所在目录，如果仍为空，则使用应用程序数据目录</param>
+    /// <param name="root">根元素，如果为空，则使用使用index.xml构建</param>
+    /// <returns></returns>
+    public static ITagsProject MakeProject(this IServiceProvider sp, string? dir = null, XElement? root = null)
+    {
+        var factory = sp.GetRequiredService<ITagsProjectFactory>();
+
+        if (string.IsNullOrEmpty(dir))
+        {
+            var loc = Assembly.GetExecutingAssembly().Location;
+            dir = Path.GetDirectoryName(loc);
+        }
+        if (string.IsNullOrEmpty(dir))
+        {
+            dir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        }
+        var proj = factory.Create(dir!, root);
+        return proj;
+    }
+
 }
