5d5c00d7c1ec9b1006395e7bb04d8e40aceeb8ca
newbienewbie <itminus@hotmail.com>
2026-07-17 08:02:22 +0800

    相关线程一律以IsBackground=true执行

--- src/Itminus.Tags.ComScanner/Channels/ComScannerChannel.cs
+++ src/Itminus.Tags.ComScanner/Channels/ComScannerChannel.cs
@@ -63,6 +63,7 @@ public class ComScannerChannel : ITagChannel
             this._channel = Channel.CreateBounded<string>(Capacity);
             // 启动轮询
             var t = new Thread(async() => await PollDataAsync(ct));
+            t.IsBackground = true;
             t.Start();
         }
         finally
